d4b626b8ae234aaf2f4a4126734ef62e05fc55b4
Amber van der Riet <110250@st-maartenscollege.nl>
2023-04-13 15:33:17 +0200

    database geconnect

--- Hotel-/Controllers/HomeController.cs
+++ Hotel-/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using Hotel_.Models;
+﻿using MySql.Data;
+using Hotel_.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
@@ -6,6 +7,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Hotel_.Database.Database;
 
 namespace Hotel_.Controllers
 {
@@ -20,7 +22,20 @@ namespace Hotel_.Controllers
 
         public IActionResult Index()
         {
-            return View();
+            // alle producten ophalen
+            var rows = DatabaseConnector.GetRows("select * from product");
+            
+            // lijst maken om alle namen in te stoppen
+            List<string> names = new List<string>();
+
+            foreach (var row in rows)
+            {
+                // elke naam toevoegen aan de lijst met namen
+                names.Add(row["naam"].ToString());
+            }
+
+            // de lijst met namen in de html stoppen
+            return View(names);
         }
 
         public IActionResult Privacy()
